865d4c65e7e766d08cb56f4fdc6df59cdaa7b725
joamla96 <21576227+joamla96@users.noreply.github.com>
2023-02-19 18:32:11 +0100

    Finalize generate icons

--- Assets/Editor/GenerateIconsTool.cs
+++ Assets/Editor/GenerateIconsTool.cs
@@ -1,12 +1,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 namespace Eco.Client
 {
@@ -18,6 +20,7 @@ namespace Eco.Client
 
         private static readonly string _iconsPath = "Assets/Images";
         private static readonly string _iconsFiletype = ".png";
+        private static readonly string _iconSuffix = "Image";
 
         [MenuItem("Tools/Generate Icons")]
         private static void GenerateIcons()
@@ -28,17 +31,30 @@ namespace Eco.Client
             var prefabTemplate = AssetDatabase.LoadAssetAtPath(_prefabTemplatePath, typeof(GameObject));
 
             CreateIcons(scene, icons, prefabTemplate);
+            
         }
 
-        private static void CreateIcons(Scene scene, List<AssetImporter> icons, Object prefabTemplate)
+        private static void CreateIcons(Scene scene, List<TextureImporter> icons, Object prefabTemplate)
         {
             var rootObjects = scene.GetRootGameObjects();
             var itemsObjects = rootObjects.First(x => x.GetScenePath() == _sceneRootParentName);
 
-            var iconGO = PrefabUtility.InstantiatePrefab(prefabTemplate, scene) as GameObject;
-            PrefabUtility.UnpackPrefabInstance(iconGO, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+            foreach (var icon in icons)
+            {
+                var iconGO = PrefabUtility.InstantiatePrefab(prefabTemplate, scene) as GameObject;
+                PrefabUtility.UnpackPrefabInstance(iconGO, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
 
-            iconGO.transform.SetParent(itemsObjects.transform, false);
+                iconGO.transform.SetParent(itemsObjects.transform, false);
+                iconGO.name = GetAssetName(icon.assetPath) + _iconSuffix;
+
+                var imageComponent = iconGO.GetComponent<UnityEngine.UI.Image>();
+                imageComponent.sprite = (Sprite)AssetDatabase.LoadAssetAtPath(icon.assetPath, typeof(Sprite));
+            }
+        }
+
+        private static string GetAssetName(string AssetPath)
+        {
+            return AssetPath.Split('/').Last().Replace(_iconsFiletype, "");
         }
 
         private static Scene PrepareScene()
@@ -58,13 +74,13 @@ namespace Eco.Client
             return scene;
         }
 
-        private static List<AssetImporter> ImportIcons()
+        private static List<TextureImporter> ImportIcons()
         {
             AssetDatabase.Refresh();
 
             var iconsToGenerate = Directory.GetFiles(_iconsPath).Where(x => x.EndsWith(_iconsFiletype)).ToList();
 
-            var assets = new List<AssetImporter>();
+            var assets = new List<TextureImporter>();
             foreach (var icon in iconsToGenerate)
             {
                 var asset = AssetImporter.GetAtPath(icon) as TextureImporter;
